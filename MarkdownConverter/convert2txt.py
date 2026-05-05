8ec26404354b8f87ddf53b0c7a97cfcf06ab83ff
NKerkfeld <s6nikerk@uni-bonn.de>
2026-05-05 11:25:54 +0200

    markdown

---
+++ MarkdownConverter/convert2txt.py
@@ -0,0 +1,48 @@
+import argparse
+import glob
+import os
+from os.path import isfile
+
+from marker.converters.pdf import PdfConverter
+from marker.models import create_model_dict
+from marker.output import text_from_rendered
+
+from tqdm import tqdm
+
+
+def to_text(path: str) -> str:
+    converter = PdfConverter(
+        artifact_dict=create_model_dict(),
+    )
+    rendered = converter(path)
+    text, _, images = text_from_rendered(rendered)
+
+    return text
+
+
+def main(input: str, output: str) -> None:
+
+    if isfile(input):
+        files = [input]
+    else:
+        files = glob.glob(f"./{input}/*.pdf")
+
+    for file in tqdm(files, total=len(files), desc="Converting..."):
+        name = os.path.basename(file)[:-4]
+
+        text = to_text(file)
+
+        os.makedirs(f"./{output}", exist_ok=True)
+        with open(f"./{output}/{name}.md", "w") as text_file:
+            text_file.write(text)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Process some file paths.")
+
+    parser.add_argument('--input', type=str, required=True, help="Path to the input file of folder with inputs files")
+    parser.add_argument('--output', type=str, required=True, help="Path to the output folder")
+
+    args = parser.parse_args()
+
+    main(args.input, args.output)
---
+++ MarkdownConverter/markdown_request.py
@@ -0,0 +1,98 @@
+import glob
+import os
+import re
+import time
+
+from tqdm import tqdm
+
+from dotenv import load_dotenv
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+from core.Client import APIClient
+from postprocess import add2csv
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+
+def extract_xml(string: str):
+    pattern = r"(<[a-z]+>[\s\S]*<\/[a-z]+>)"
+    matches = re.findall(pattern, string)
+
+    return " ".join(matches)
+
+
+@hydra.main(version_base=None, config_path="../config", config_name="config")
+def main(cfg: DictConfig):
+    print(OmegaConf.to_yaml(cfg))
+
+    os.makedirs(f"{cfg.output}/message/", exist_ok=True)
+    os.makedirs(f"{cfg.output}/xml/", exist_ok=True)
+    os.makedirs(f"{cfg.output}/response/", exist_ok=True)
+
+    with open(f"{cfg.output}/log.txt", "w") as text_file:
+        text_file.write("Configurations:")
+        text_file.write(OmegaConf.to_yaml(cfg))
+        text_file.write("\n")
+
+    client = APIClient(TOKEN,
+                       model=cfg.model,
+                       server=cfg.server,
+                       timeout=cfg.timeout)
+
+    with open(cfg.prompt, "r") as path:
+        request = path.read()
+
+    files = list(glob.glob(f"{cfg.input}/*.md"))
+    successful = 0
+    failed = []
+
+    bar = tqdm(files)
+    for path in bar:
+        file = os.path.basename(path)
+        bar.set_description(f"{file[:-4]} Successful: {successful}/{successful + len(failed)}")
+
+        with open(path, "r") as md_file:
+            document = md_file.read()
+
+        try:
+            response = client.request(f"{request}\n\n{document}\n\n{request}")
+            message = response['choices'][0]['message']['content']
+
+            with open(f"{cfg.output}/message/{file[:-4]}.txt", "w") as text_file:
+                text_file.write(message)
+
+        except Exception as e:
+            with open(f"{cfg.output}/log.txt", "a") as text_file:
+                text_file.write(f"Got this Exception with {file} during the request:\n")
+                text_file.write(str(e))
+                text_file.write("\n")
+                failed.append(file)
+                continue
+
+        try:
+            xml_response = extract_xml(message)
+            with open(f"{cfg.output}/xml/{file[:-4]}_xml.txt", "w") as text_file:
+                text_file.write(xml_response)
+
+            add2csv(xml_response, f"{cfg.output}/output.csv", file[:-4])
+        except Exception as e:
+            with open(f"{cfg.output}/log.txt", "a") as text_file:
+                text_file.write(f"Got this Exception with {file} during the xml extraction:")
+                text_file.write(str(e))
+                text_file.write("\n")
+                failed.append(file)
+                continue
+
+        successful += 1
+        bar.set_description(f"{file[:-4]} Successful: {successful}/{successful + len(failed)}")
+        time.sleep(1)
+
+    print(f"Process completed.")
+    print(f"Successful: {successful}/{len(files)}")
+    print(f"Failed: {', '.join(failed)}")
+
+
+if __name__ == '__main__':
+    main()
