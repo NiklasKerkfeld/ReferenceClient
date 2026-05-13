feb28dbf59bbc2ae9eace2fe0c37e62d239fcb62
NKerkfeld <s6nikerk@uni-bonn.de>
2026-05-13 15:02:53 +0200

    update markdown script

--- MarkdownConverter/markdown_request.py
+++ MarkdownConverter/markdown_request.py
@@ -1,7 +1,7 @@
 import glob
+import json
 import os
-import re
-import time
+from os.path import isfile
 
 from tqdm import tqdm
 
@@ -10,20 +10,13 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 
 from core.Client import APIClient
-from postprocess import add2csv
+from postprocess import add2csv, extract_xml
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
 
-def extract_xml(string: str):
-    pattern = r"(<[a-z]+>[\s\S]*<\/[a-z]+>)"
-    matches = re.findall(pattern, string)
-
-    return " ".join(matches)
-
-
-@hydra.main(version_base=None, config_path="../config", config_name="config")
+@hydra.main(version_base=None, config_path="config", config_name="config")
 def main(cfg: DictConfig):
     print(OmegaConf.to_yaml(cfg))
 
@@ -51,18 +44,16 @@ def main(cfg: DictConfig):
     bar = tqdm(files)
     for path in bar:
         file = os.path.basename(path)
-        bar.set_description(f"{file[:-4]} Successful: {successful}/{successful + len(failed)}")
 
         with open(path, "r") as md_file:
             document = md_file.read()
 
+        if isfile(f"{cfg.output}/message/{file[:-4]}.txt"):
+            continue
+
         try:
             response = client.request(f"{document}\n\n{request}")
             message = response['choices'][0]['message']['content']
-
-            with open(f"{cfg.output}/message/{file[:-4]}.txt", "w") as text_file:
-                text_file.write(message)
-
         except Exception as e:
             with open(f"{cfg.output}/log.txt", "a") as text_file:
                 text_file.write(f"Got this Exception with {file} during the request:\n")
@@ -71,12 +62,18 @@ def main(cfg: DictConfig):
                 failed.append(file)
                 continue
 
+        with open(f"{cfg.output}/message/{file[:-4]}.txt", "w") as text_file:
+            text_file.write(message)
+
+        with open(f"{cfg.output}/response/{file[:-4]}.json", 'w', encoding='utf-8') as f:
+            json.dump(response, f, ensure_ascii=False, indent=4)
+
         try:
             xml_response = extract_xml(message)
-            with open(f"{cfg.output}/xml/{file[:-4]}_xml.txt", "w") as text_file:
+            if xml_response == "": raise Exception("xml string is empty")
+            with open(f"{cfg.output}/xml/{file[:-4]}.xml", "w") as text_file:
                 text_file.write(xml_response)
 
-            add2csv(xml_response, f"{cfg.output}/output.csv", file[:-4])
         except Exception as e:
             with open(f"{cfg.output}/log.txt", "a") as text_file:
                 text_file.write(f"Got this Exception with {file} during the xml extraction:")
@@ -85,9 +82,18 @@ def main(cfg: DictConfig):
                 failed.append(file)
                 continue
 
+        try:
+            add2csv(xml_response, f"{cfg.output}/output.csv", file[:-4])
+        except Exception as e:
+            with open(f"{cfg.output}/log.txt", "a") as text_file:
+                text_file.write(f"Got this Exception with {file} during the csv extraction:")
+                text_file.write(str(e))
+                text_file.write("\n")
+                failed.append(file)
+                continue
+
         successful += 1
         bar.set_description(f"{file[:-4]} Successful: {successful}/{successful + len(failed)}")
-        time.sleep(1)
 
     print(f"Process completed.")
     print(f"Successful: {successful}/{len(files)}")
